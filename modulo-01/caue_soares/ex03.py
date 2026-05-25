516aea3cc72eb0e684d7e01c9e96f5944b98c354
cauesoares721-oss <Cauesoares721@gmail.com>
2026-05-25 19:02:03 -0400

    adicioanar exercicios

---
+++ modulo-01/caue_soares/ex03.py
@@ -0,0 +1,5 @@
+import this
+
+# Princípio que mais fez sentido: "Readability counts."
+# Código é lido muito mais vezes do que escrito. Escrever de forma clara e
+# legível economiza tempo de todos que precisarem entender ou manter o código.
